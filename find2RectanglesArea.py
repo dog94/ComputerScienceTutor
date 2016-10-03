ef151a44aed6f306ee57c9dfa81148189d321f31
dog94 <dog94@163.com>
2016-10-03 12:28:58 -0700

    leetcode problem, find the total area of 2 rectangles in a 2D plane.

---
+++ find2RectanglesArea.py
@@ -0,0 +1,46 @@
+"""
+Find the total area covered by two rectilinear rectangles in a 2D plane.
+
+Each rectangle is defined by its bottom left corner and top right corner.
+
+        -------- (C,D)
+        |      |
+        |  ------- (G,H)
+        |  |   | |
+        |  |   | |
+  (A,B) ---|---- |
+           |     |
+     (E,F) -------
+"""
+
+
+
+class Solution(object):
+    def computeArea(self, A, B, C, D, E, F, G, H):
+        """
+        :type A: int
+        :type B: int
+        :type C: int
+        :type D: int
+        :type E: int
+        :type F: int
+        :type G: int
+        :type H: int
+        :rtype: int
+        """ 
+        # when one of the rectangle has area of 0
+        if((D-B)*(A-C)==0):
+            return (G-E)*(H-F)
+        if((G-E)*(H-F)==0):
+            return (A-C)*(B-D)
+            
+        # No Overlapping
+        if(max(A,C)<min(E,G) or max(E,G)<min(A,C) or max(B,D)<min(H,F)or max(H,F)<min(B,D)):
+            return ((A-C)*(B-D) + (G-E)*(H-F))
+            
+        x=[A,C,E,G]
+        y=[B,D,F,H]
+        x.sort()
+        y.sort()
+        
+        return (G-E)*(H-F)+(A-C)*(B-D)-(x[2]-x[1])*(y[2]-y[1])
